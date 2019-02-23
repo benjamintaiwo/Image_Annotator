8bf0a1cef17db70b9ed1b3557715221dd2890a90
benjamintaiwo <abegundetaiwo@gmail.com>
2019-02-23 23:17:44 +0100

    Edit: settings

--- img_annotator/settings.py
+++ img_annotator/settings.py
@@ -126,11 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_dev')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dev'),
-)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
